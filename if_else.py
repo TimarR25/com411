fcb717ebf404602a0783428e8475cad44f2e0f9f
TimarR25 <6timar27@solent.ac.uk>
2022-10-14 10:43:09 +0100

    if_2

---
+++ if_else.py
@@ -0,0 +1,8 @@
+print("Please enter the activity to be performed")
+
+activity = input("calculate")
+
+if activity == "calculate":
+    input("Performing calculations...")
+
+input("Activity Complete!")
